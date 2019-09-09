e700b2f27ab9347388f188206993f651de74a66b
Gomez Martin <daniel.gomez@realtech.es>
2019-09-09 13:10:55 +0200

    Error port

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,7 +40,7 @@ import { AdminModule } from './hades-admin/admin.module';
         // extra: {
         //   socketPath: config.get('DATABASE_SOCKET'),
         // },
-        port: '3306',
+        port: 3306,
         host: '172.17.0.1',
         username: config.get('DATABASE_USER'),
         password: config.get('DATABASE_PASSWORD'),
