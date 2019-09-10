f4326125f6491e515b6d1de22c4d2b33080b3800
Gomez Martin <daniel.gomez@realtech.es>
2019-09-10 09:48:59 +0200

    Change IP

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,7 +39,7 @@ import { AdminModule } from './hades-admin/admin.module';
         name: 'default',
         type: 'mysql' as 'mysql',
         port: 3306,
-        host: 'localhost',
+        host: '172.17.0.1',
         username: config.get('DATABASE_USER'),
         password: config.get('DATABASE_PASSWORD'),
         database: 'sys',
